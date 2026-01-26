f325296c6dd0a479f3d36a3cebf80834b276bdbd
Josue herazo <josuedh18@hotmail.es>
2026-01-26 03:27:49 +0100

    agregando page list dates

--- src/services/ServiceDates.ts
+++ src/services/ServiceDates.ts
@@ -9,7 +9,7 @@ type serviceData = {
 }
 export async function addProduct(data : serviceData) {
     try {
-        console.log("Esquema actual:", DraftDateSchema); // Esto te dirá si ya cambió a string
+        
         const priceNumber = Number(data.price);
         // const dateListNumber = Number(data.dateList);
         // VALIBOT LIMPIA LOS DATOS Y PARSEA EL TYPE
@@ -31,15 +31,15 @@ if (result.success) {
     // SE CREA LA RUTA DE DESTINO
     const url = `${import.meta.env.VITE_API_URL}/api/date`
     // LUEGO SE ENVIA LA DATA A LA SERVER CON EL METODO POST Y SE AÑADE LA URL LUEGO LA DATA YA VALIDADA
-  console.log("ERRORES DE VALIBOT:", result.issues);
+    const dateObject = new Date(result.output.dateList);
      await axios.post(url, 
         {
             barber: result.output.barber,
             service: result.output.service,
             price: +result.output.price,
-            dateList: result.output.dateList.toISOString().split('T')[0], // Formatear a "YYYY-MM-DD"
+            dateList: dateObject.toISOString(), // Formatear a "YYYY-MM-DD"
             client: result.output.client,   
-            phone: result.output.phone,
+            phone: +result.output.phone,
             
             
             
