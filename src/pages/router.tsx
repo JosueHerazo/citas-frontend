a5d18ac2e72184266911891a2f131a5a97c9bc48
unknown <josueherazo25@gmail.com>
2026-04-02 03:23:41 +0200

    fix

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layouts/Layout";
-import NewDate, { action as listDateAction } from "./NewDate";
+import NewDate, { action as newDateAction } from "./NewDate";
 import Login, { action as loginAction } from "../pages/Login";
 import { ProtectedRoute } from "../pages/ProtectedRoute";
 import Register, { action as registerAction } from "../pages/Register";
@@ -20,7 +20,7 @@ export const router = createBrowserRouter([
       {
         index: true,
         element: <NewDate />,
-        action: listDateAction,
+        action: newDateAction,
       },
       {
         path: "nuevo/inicio",
